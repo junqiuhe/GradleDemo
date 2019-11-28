5a360873d270a91bb4d840850e58643ffe9ddcad
hejunqiu <13980043975@qfang.com>
2019-11-28 10:25:26 +0800

    Resolve Problem
    description: A library dependency includes a flavor dimension that your app does not.

---
+++ testapp/src/main/java/com/sample/gradle/demo/test/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sample.gradle.demo.test;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Project Name：GradleDemo
+ * Created by hejunqiu on 2019/11/28 9:32
+ * Description:
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
