652510d1956ddaeb9e4662d53899ad3cba655f3b
hejunqiu <13980043975@qfang.com>
2019-11-28 15:08:23 +0800

    Add build multiple apks demo

---
+++ multipleapp/src/main/java/com/sample/demo/multiple/app/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sample.demo.multiple.app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Project Name：GradleDemo
+ * Created by hejunqiu on 2019/11/28 11:02
+ * Description:
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
